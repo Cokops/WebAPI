b3665b0021465cc667b3c557371cab510470617e
Artem <gerasev-z@mail.ru>
2025-06-30 15:22:15 +0300

    Первое подключение к Mongo

---
+++ WebAPI/Core/WorkFile.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebAPI.Core
+{
+    public class WorkFile
+    {
+        [BsonId]
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime LastCheck { get; set; }
+        public DateTime EndCheck { get; set; }
+
+    }
+}
---
+++ WebAPI/Infrastructure/MongoDataBase.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System.Diagnostics.Metrics;
+using WebAPI.Core;
+
+namespace WebAPI.Infrastructure
+{
+    public class MongoDataBase
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDataBase(IConfiguration config)
+        {
+            var client = new MongoClient(config["MongoDBSettings:ConnectionString"]);
+            _database = client.GetDatabase(config["MongoDBSettings:DatabaseName"]);
+        }
+
+        public IMongoCollection<WorkFile> WorkFile => _database.GetCollection<WorkFile>("IDK");
+    }
+}
